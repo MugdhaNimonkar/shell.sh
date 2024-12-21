99c0c90395aea70232334f16c9f06efdb0df7a7d
Mugdha Nimonkar <msnimonkar@gmail.com>
2024-12-21 18:59:28 +0530

    initial commit

---
+++ src/main/java/com/cdac/acts/RegisterServlet.java
@@ -0,0 +1,32 @@
+package com.cdac.acts;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		@SuppressWarnings("unused")
+		PrintWriter out = response.getWriter();
+		@SuppressWarnings("unused")
+		String userName = request.getParameter("userName");
+		@SuppressWarnings("unused")
+		String password = request.getParameter("password");
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
